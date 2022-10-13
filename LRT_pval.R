35d50ee65274b7ad388f897e43cd4cdc2ff35bd7
karamveerverma37 <31627303+karamveerverma37@users.noreply.github.com>
2022-10-13 23:20:30 +0530

    Update LRT_pval.R

--- LRT_pval.R
+++ LRT_pval.R
@@ -1,7 +1,7 @@
 # input table is parsed results from batch_pairwise.hyphy
-`data <- read.csv("table.tsv", sep = "\t")`
-`LRT<-data$lrt`
-`pVal <- 1-pchisq(LRT,1)`
-`x<-cbind(data,pVal)`
+data <- read.csv("table.tsv", sep = "\t")
+LRT<-data$lrt
+pVal <- 1-pchisq(LRT,1)
+x<-cbind(data,pVal)
 # output is input table with p-value information
-`write.table(x,file='output_pVal.csv',sep = "\t",row.names = F)`
+write.table(x,file='output_pVal.csv',sep = "\t",row.names = F)
